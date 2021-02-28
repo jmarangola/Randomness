a0160a3644adcea52d360db4128c6475cb0a7eda
John Marangola <john.marangola@gmail.com>
2021-02-28 17:47:35 -0500

    updating hw2 code

--- Randomness/venken.py
+++ Randomness/venken.py
@@ -1,11 +1,30 @@
 import math
 import scipy.special as sp
 
-def b(n, p, k):
-    return sp.binom(n, k) * pow(p, k) * pow(p, n-k)
+N = 100
+p = 0.5
 
-def test_range():
-    pass
+
+def b(n, p, k):
+    return sp.comb(n, k) * math.pow(p, k) * math.pow((1-p), n-k)
+
+def get_zscore(x):
+    mean = N*p
+    sigma = math.sqrt(N*p*(1-p))
+    return (x-mean)/sigma
+    
+def p_greater(xlower):
+    alpha = 0.0
+    for i in range(xlower, N):
+        alpha += b(N, p, i)
+    return alpha
 
 if __name__ == "__main__":
-    print(b(6, 0.5, 3))
+    print(p_greater(61))
+        
+    
+    
+    
+    
+
+        
