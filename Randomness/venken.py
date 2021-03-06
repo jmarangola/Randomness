b6c4d3d82195e3245c0bb05964bf57a01da30a83
John Marangola <john.marangola@gmail.com>
2021-03-05 22:59:05 -0500

    finishing p2

--- Randomness/venken.py
+++ Randomness/venken.py
@@ -2,13 +2,11 @@ import math
 import scipy.special as sp
 
 N = 100
-p = 0.5
-
 
 def b(n, p, k):
     return sp.comb(n, k) * math.pow(p, k) * math.pow((1-p), n-k)
 
-def get_zscore(x):
+def get_zscore(x, p):
     mean = N*p
     sigma = math.sqrt(N*p*(1-p))
     return (x-mean)/sigma
@@ -20,7 +18,29 @@ def p_greater(xlower):
     return alpha
 
 if __name__ == "__main__":
-    print(p_greater(61))
+    i = 0
+    alpha, beta = 0, 0
+    for i in range(N):
+        alpha = 0.0
+        for j in range(i, N):
+            alpha += b(N, 0.5, j)
+        if alpha < 0.05:
+            print(f"m: {i} for alpha")
+            break
+    i = N
+    for i in range(N, 0, -1):
+        alpha = 0.0
+        for j in range(i, N):
+            alpha += b(N, 0.7, j)
+        if 1-alpha < 0.05:
+            print(f"beta: {1-alpha}, i: {i}")
+            break
+            
+
+        
+        
+        
+        
         
     
     
