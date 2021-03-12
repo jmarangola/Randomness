e1a57f4ec7ac9cc6a648e1d712cb19e2d75626e0
John Marangola <john.marangola@gmail.com>
2021-03-12 18:59:25 -0500

    hw3

---
+++ Randomness/hw3/monty_python.py
@@ -0,0 +1,9 @@
+import random
+import math
+
+
+if __name__ == "__main__":
+    potential_reveals = [x for x in range(3)]
+    print(potential_reveals)
+    winning_cat = random.randint(1, 3)
+    user_initial_choice = random.randint(1, 3)
