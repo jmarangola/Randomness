c5de1cc160ac1b905a2a2a6aaaf7a1d1527e6d69
John Marangola <john.marangola@gmail.com>
2021-03-12 19:12:48 -0500

    Monte carlo simulation foundation without set

--- Randomness/hw3/monty_python.py
+++ Randomness/hw3/monty_python.py
@@ -2,8 +2,25 @@ import random
 import math
 
 
+def run_simulation(n):
+    switch_tally, stay_tally = {"total":0, "correct":0}, {"total":0, "correct":0}
+    for i in range(n):
+        winning_cat = random.randint(1, 3)
+        user_initial_choice = random.randint(1, 3)
+        potential_reveals = []
+        for i in range(1, 4):
+            if  i == user_initial_choice or i == winning_cat:
+                continue
+            potential_reveals.append(i)
+        revealed = random.choice(potential_reveals)
+        switch = random.range(0, 1)
+        if switch:
+            switch_tally["total"] += 1
+            
+        else:
+            stay_tally["total"] += 1
+    
 if __name__ == "__main__":
-    potential_reveals = [x for x in range(3)]
-    print(potential_reveals)
-    winning_cat = random.randint(1, 3)
-    user_initial_choice = random.randint(1, 3)
+    run_simulation(1000)
+    
+            
