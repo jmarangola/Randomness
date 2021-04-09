fb178c9aa805a74a97973a48f3716c112a00a7c7
John Marangola <john.marangola@gmail.com>
2021-04-09 03:15:34 -0400

    final homework result with single randomized list

--- Randomness/hw3/quickselect.py
+++ Randomness/hw3/quickselect.py
@@ -1,25 +1,26 @@
+"""
+    Author: John Marangola - marangol@bc.edu
+    4/9/2021
+    Professor McTague
+    Randomness and Computation
+    Programming Assignment 2
+"""
 import random
 
-counter = 0
+# Single Randomized Pivot Quickselection:
 def quickselect(l, k):
     global counter
     counter += 1
     length = len(l)
-    # Pick a random pivot element from the list, each
-    # equally likely
     pivot = random.choice(l)
     l_small = []
     l_big = []
-    # Put all elements smaller than pivot into l_small, and all
-    # larger elements into l_big.
     for element in l:
         if element < pivot:
             l_small.append(element)
         elif element > pivot:
             l_big.append(element)
-    # We assume all elements are distinct, so (besides the pivot) every element# should go into l_small or l_big
     assert(length == len(l_small) + len(l_big) + 1)
-    
     if k <= len(l_big):
     # kth largest must be in l_big
         res = quickselect(l_big, k)
@@ -31,7 +32,7 @@ def quickselect(l, k):
     else:
         return pivot
     
-# Two pivot quickselect variant
+# Double randomized pivot Quickselect variation
 def double_variant(l, k):
     global counter
     counter += 1
@@ -47,38 +48,44 @@ def double_variant(l, k):
             mid.append(element)
         elif element > max_pivot:
             larger.append(element)
-            
-    # perform quickselect variation with two pivots
+    # Perform quickselect variation with two pivots
     len_smaller, len_mid, len_larger = len(smaller), len(mid), len(larger)
-    if len_larger == k-1:
+    if len_larger == k - 1:
         return max_pivot
     elif len_larger + len_mid == k - 2:
         return min_pivot
     elif k <= len_larger:
-        # its in the larger subarray
+        # If the kth value is in the 'larger' subarray
         return double_variant(larger, k)
     elif k > len_larger + len_mid + 2:
-        # recurse smaller subarray
+        # If the kth value is in the 'smaller' subarray
         return double_variant(smaller, k - (len_larger + len_mid + 2))
     else:
-        # recurse mid subarray
+        # If the kth value is in the 'mid' subarray
         return double_variant(mid, k - (len_larger + 1))
 
-
- 
-        
-            
-            
-    
 if __name__ == "__main__":
+    # Simulation Parameters -----
     n_elements = 125
     k = 50
     n_simulations = 20000
     n_pivots = 2
-    print("Beginning Monte Carlo Simulation...")
+    randomizedElements = True
+    # ----------------------------
     
-    l = list(range(1, n_elements+1))
+    # Ability to select non-randomized list elements
+    if not randomizedElements:
+        l = list(range(1, n_elements+1))
+    else:  # Create randomized list of n_elements values
+        l = []
+        potential_choices = list(range(n_elements))
+        for i in range(n_elements):
+            choice = random.choice(potential_choices)
+            potential_choices.remove(choice)
+            l.append(choice)
     sum_ = 0 
+    # Run Monte-Carlo Simulation
+    print("Beginning Monte Carlo Simulation...")
     if n_pivots == 1:
         for i in range(n_simulations):
             counter = 0
@@ -93,7 +100,3 @@ if __name__ == "__main__":
         print(sum_/n_simulations)
     else:
         print("Error, invalid number of pivots selected")
-        
-        
-    
-    
