2359f881c56aef3ee51311bd16c0639201abc509
John Marangola <john.marangola@gmail.com>
2021-04-28 19:11:09 -0400

    gsr

--- Randomness/Programming Assignments/P3/riffle_shuffle.py
+++ Randomness/Programming Assignments/P3/riffle_shuffle.py
@@ -9,8 +9,26 @@ import random
 
 # Gilbert-Shannon-Reeds model to simulate human riffle shuffling
 def gsr(l):
+    size = len(l)
     cards = l.copy()
-
+    random_seq, left, right, riffle = [], [], [], []
+    n_right = 0
+    for i in range(size):
+        flip = random.randint(0, 1)
+        if flip: 
+            n_right += 1        
+        random_seq.append(flip)
+    # Cut the deck into left and right halves:
+    left = [cards[i] for i in range(size - n_right)]
+    right  = [cards[i] for i in range(size - n_right, size)]
+    for x in range(size):
+        if random_seq[x]:
+            riffle.append(right.pop(0))
+        else:
+            riffle.append(left.pop(0))
+    return riffle
+    
+    
 # Top-to-Random shuffle, a simplistic shuffling method placing top card in a random position
 def top_to_random(l):
     cards = l.copy()
@@ -19,4 +37,4 @@ def top_to_random(l):
     return cards
 
 if __name__ == "__main__":
-    x = list(range(10))
+    pass
